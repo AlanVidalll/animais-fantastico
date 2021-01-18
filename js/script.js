0f40acf8217a3e50cd2a27c0c6ead309e53e6a16
Alan Vidal <alanfonseca7812@gmail.com>
2021-01-18 07:22:37 -0300

    Posição scroll animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 import ScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tab-nav.js'
 import initModal from './modules/modal.js'
@@ -10,11 +9,10 @@ import initMenuMobile from './modules/menu-mobile.js'
 import initFuncionamento from './modules/funcionamento.js'
 import initFetchAniamis from './modules/fetch-animais.js'
 import initFetchBiticoin from './modules/fetch-biticoin.js'
-
+import initAnimacaoScroll from './modules/scroll-animacao.js'
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^= "#"]');
 scrollSuave.init();
-initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -24,3 +22,4 @@ initMenuMobile();
 initFuncionamento();
 initFetchAniamis();
 initFetchBiticoin();
+initAnimacaoScroll();
