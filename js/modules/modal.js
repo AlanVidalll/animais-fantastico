1f871125da69885dfd2a298410a0190629bef58b
Alan Vidal <alanfonseca7812@gmail.com>
2021-01-26 09:40:25 -0300

    modal transforma em classe

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,25 +1,46 @@
-export default function initModal() {
+export default class Modal {
 
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]')
-  const botaoFechar = document.querySelector('[data-modal="fechar"]')
-  const containerModal = document.querySelector('[data-modal="container"]')
+  constructor(btnAbrir, btnFechar, containerModal) {
+    this.botaoAbrir = document.querySelector(btnAbrir)
+    this.botaoFechar = document.querySelector(btnFechar)
+    this.containerModal = document.querySelector(containerModal)
 
+    /*bind this ao callback para 
+    fazer refência ao objeto da classe*/
 
-  if (botaoAbrir && botaoFechar && containerModal) {
+    this.eventoToggleModal = this.eventoToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
+  }
+  // abre ou fecha o modal.
+  toggleModal() {
+    this.containerModal.classList.toggle('ativo');
+  }
 
-    function toggleModal(event) {
-      event.preventDefault();
-      containerModal.classList.toggle('ativo');
+  // adiciona o evento de toggle ao modal
+  eventoToggleModal(event){
+    event.preventDefault();
+    this.toggleModal();
+
+  }
+
+ // fecha o  modal ao clicar do lado de fora.
+  cliqueForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal();
     }
+  }
 
-    function cliqueForaModal(event) {
-      if (event.target === this) {
-        toggleModal(event);
-      }
+ // adiciona os eventos aos elementos do modal.
+  addModalEvents() {
+    this.botaoAbrir.addEventListener('click', this.eventoToggleModal);
+    this.botaoFechar.addEventListener('click', this.eventoToggleModal);
+    this.containerModal.addEventListener('click', this.cliqueForaModal);
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addModalEvents();
     }
- 
-    botaoAbrir.addEventListener('click', toggleModal);
-    botaoFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueForaModal);
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 import ScrollSuave from './modules/scroll-suave.js'
 import Accordion from './modules/accordion.js'
 import TabNav from './modules/tab-nav.js'
-import initModal from './modules/modal.js'
+import Modal from './modules/modal.js'
 import initTolltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
@@ -21,7 +21,10 @@ accordion.init();
 const tabNav  = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
 
-initModal();
+const modal  = new Modal('[data-modal="abrir"]','[data-modal="fechar"]','[data-modal="container"]');
+modal.init();
+
+
 initTolltip();
 initDropdownMenu();
 initMenuMobile();
