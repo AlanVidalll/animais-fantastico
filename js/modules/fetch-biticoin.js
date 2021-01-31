d0667d5a46e53d092e9f1c46bc58e7a6c1f30939
Alan Vidal <alanfonseca7812@gmail.com>
2021-01-31 20:44:24 -0300

    FetchBitcoin refatorado

--- js/modules/fetch-biticoin.js
+++ js/modules/fetch-biticoin.js
@@ -1,11 +1,12 @@
-export default function initFetchBiticoin() {
+export default function fetchBiticoin(url,target) {
 
-  fetch('https://www.blockchain.com/ticker').then((response) =>
+  fetch(url).then((response) =>
     response.json()
   ).then((biticoin) => {
-    const btcPreco = document.querySelector('.btc-preco')
+    const btcPreco = document.querySelector(target)
     btcPreco.innerText = (1000 / biticoin.BRL.sell).toFixed(4);
   }).catch((error) =>{
   console.log(Error(error));
   })
 }
+
