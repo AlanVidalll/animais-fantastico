72ae89cd21c686e520a29141b2ae9fc8523e2c02
Alan Vidal <alanfonseca7812@gmail.com>
2021-02-08 20:25:49 -0300

    atualizando

--- js/modules/fetch-biticoin.js
+++ js/modules/fetch-biticoin.js
@@ -1,12 +1,11 @@
-export default function fetchBiticoin(url,target) {
+export default function fetchBiticoin(url, target) {
 
   fetch(url).then((response) =>
     response.json()
   ).then((biticoin) => {
     const btcPreco = document.querySelector(target)
     btcPreco.innerText = (1000 / biticoin.BRL.sell).toFixed(4);
-  }).catch((error) =>{
-  console.log(Error(error));
+  }).catch((error) => {
+    console.log(Error(error));
   })
 }
-
