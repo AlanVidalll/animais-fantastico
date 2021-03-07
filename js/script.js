4d5145496cd58b072fdcf899fe974930e8938364
Alan Vidal <alanfonseca7812@gmail.com>
2021-03-07 20:59:35 -0300

    Ajuste no caminho do arquivo animaisapi.json

--- js/script.js
+++ js/script.js
@@ -38,5 +38,5 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]','aberto');
 funcionamento.init();
 
-fetchAnimais('./animaisapi.json', '.numeros-grid')
+fetchAnimais('../animaisapi.json', '.numeros-grid')
 fetchBiticoin('https://www.blockchain.com/ticker', '.btc-preco');
